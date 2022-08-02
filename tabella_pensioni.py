fb9e5b6c6fd253f16ff40093816d640f496e6857
byschii <byschii@protonmail.com>
2022-08-02 08:59:54 +0200

    Create tabella_pensioni.py

---
+++ tabella_pensioni.py
@@ -0,0 +1,51 @@
+tabella_pensioni = """Da 0 a 1 volta il minimo Da 0 a 515,58            |  7.480.943 |31.746.455.366  | 4.243
+Da 1 a 2 volte il minimo Da 515,59 a 1031,16      |  7.011.368 |63.923.882.868  | 9.117
+Da 2 a 3 volte il minimo Da 1031,17 a 1546,74     |  3.438.525 |57.478.060.299  |16.715
+Da 3 a 4 volte il minimo Da 1546,75 a 2062,32     |  2.126.802 |49.471.793.053  |23.261
+Da 4 a 5 volte il minimo Da 2062,33 a 2577,90     |  1.333.133 |39.824.130.898  |29.872
+Da 5 a 6 volte il minimo Da 2577,91 a 3093,48     |  567.962   |20.684.238.560  |36.418
+Da 6 a 7 volte il minimo Da 3093,49 a 3609,06     |  270.010   |11.648.791.146  |43.142
+Da 7 a 8 volte il minimo Da 3609,07 a 4124,64     |  143.180   |7.152.049.031   |49.951
+Da 8 a 9 volte il minimo Da 4124,65 a 4640,22     |  90.233    |5.126.442.446   |56.813
+Da 9 a 10 volte il minimo Da 4640,23 a 5155,80    |  68.312    |4.338.612.927   |63.511
+Da 10 a 11 volte il minimo Da 5155,81 a 5671,38   |  55.864    |3.926.058.365   |70.278
+Da 11 a 12 volte il minimo Da 5671,39 a 6186,96   |  42.574    |3.269.474.446   |76.795
+Da 12 a 13 volte il minimo Da 6186,97 a 6702,54   |  26.473    |2.213.063.136   |83.596
+Da 13 a 14 volte il minimo Da 6702,55 a 7218,12   |  18.215    |1.643.584.691   |90.232
+Da 14 a 15 volte il minimo Da 7218,13 a 7733,70   |  13.314    |1.291.868.136   |97.030
+Da 15 a 16 volte il minimo Da 7733,71 a 8249,28  |   8.523    | 883.544.964    |103.665
+Da 16 a 17 volte il minimo Da 8249,29 a 8764,86  |   5.153    | 568.750.316    |110.372
+Da 17 a 18 volte il minimo Da 8764,87 a 9280,44  |   3.470    | 406.274.366    |117.081
+Da 18 a 19 volte il minimo Da 9280,45 a 9796,02  |   2.322    | 287.450.869    |123.794
+Da 19 a 20 volte il minimo Da 9796,03 a 10311,60 |   1.747    | 228.106.938    |130.570
+Da 20 a 21 volte il minimo Da 10311,61 a 10827,18|   1.413    | 193.936.878    |137.251
+Da 21 a 22 volte il minimo Da 10827,19 a 11342,76|   1.230    | 177.186.299    |144.053
+Da 22 a 23 volte il minimo Da 11342,77 a 11858,34|   1.107    | 166.837.457    |150.711
+Da 23 a 24 volte il minimo Da 11858,35 a 12373,92|   924      | 145.322.213    |157.275
+Da 24 a 25 volte il minimo Da 12373,93 a 12889,5 |   685      | 112.367.663    |164.040
+Da 25 a 26 volte il minimo Da 12889,51 a 13405,08  |  705   |     120.624.895  |   171.099
+Da 26 a 27 volte il minimo Da 13405,09 a 13920,66  |  604   |     107.125.766  |   177.360
+Da 27 a 28 volte il minimo Da 13920,67 a 14436,24  |  405   |     74.686.092   |   184.410
+Da 28 a 29 volte il minimo Da 14436,25 a 14951,82  |  328   |     62.534.856   |   190.655
+Da 29 a 30 volte il minimo Da 14951,83 a 15467,40  |  219   |     43.246.517   |   197.472
+Da 30 a 31 volte il minimo Da 15467,41 a 15982,98  |  198   |     40.433.716   |   204.210
+Da 31 a 32 volte il minimo Da 15982,99 a 16498,56  |  171   |     36.066.367   |   210.914
+Da 32 a 33 volte il minimo Da 16498,57 a 17014,14  |  136   |     29.602.599   |   217.666
+Da 33 a 34 volte il minimo Da 17014,15 a 17529,72  |  82    |     18.385.456   |   224.212
+Da 34 a 35 volte il minimo Da 17529,73 a 18045,30  |  85      |   19.614.545    |  230.759
+Da 35 a 36 volte il minimo Da 18045,31 a 18560,88  |  73      |   17.356.749    |  237.763
+Da 36 a 37 volte il minimo Da 18560,89 a 19076,46  |  51      |   12.476.182    |  244.631
+Da 37 a 38 volte il minimo Da 19076,47 a 19592,04  |  49      |   12.314.158    |  251.309
+Da 38 a 39 volte il minimo Da 19592,05 a 20107,62  |  45      |   11.601.451    |  257.810
+Da 39 a 40 volte il minimo Da 20107,63 a 20623,20  |  38      |   10.046.626    |  264.384
+Da 40 a 41 volte il minimo Da 20623,21 a 21138,78  |  38      |   10.292.547    |  270.856
+Da 41 a 42 volte il minimo Da 21138,79 a 21654,36  |  29      |   8.081.321     |  278.666
+Da 42 a 43 volte il minimo Da 21654,37 a 22169,94  |  21      |   5.988.470     |  285.165
+Da 43 a 44 volte il minimo Da 22169,95 a 22685,52  |  23    |     6.705.250    |   291.532
+Da 44 a 45 volte il minimo Da 22685,53 a 23201,10  |  27    |     8.046.856    |   298.031
+Da 45 a 46 volte il minimo Da 23201,11 a 23716,68  |  22    |     6.710.944    |   305.042
+Da 46 a 47 volte il minimo Da 23716,69 a 24232,26  |  17    |     5.285.259    |   310.897
+Da 47 a 48 volte il minimo Da 24232,27 a 24747,84  |  15    |     4.786.063    |   319.070
+Da 48 a 49 volte il minimo Da 24747,85 a 25263,42  |  15    |     4.879.178    |   325.278
+Da 49 a 50 volte il minimo Da 25263,43 a 25779,00  |  22    |     7.290.882    |   331.403
+Oltre 50 volte il minimo Oltre 25779,00         |     220      |  97.210.389  |    441.865"""
