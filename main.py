b9bb73e82a1eb10263955902fe4ee55d5b4af7d6
byschii <byschii@protonmail.com>
2022-08-02 09:02:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit
 import tabella_pensioni as tb
 
+import numpy as np
+import pandas as pd
 
+pensioni_parsed = []
+for riga in tb.tabella_pensioni.split("\n"):
+    r = list(map(lambda x:int(x.strip().replace(".","")), riga.split("|")[1:]))
+    pensioni_parsed.append(r)
 
-streamlit.write(tb.tabella_pensioni)
+pensioni_parsed = np.array(pensioni_parsed)
+streamlit.write(pd.DataFrame(pensioni_parsed))
+
